8b004cc49a4bbb2726ed789a6cbb10ed341df99f
BigXplosion <bigXplosion44@gmail.Com>
2015-12-17 10:12:03 +0100

    Trying to fix a weird bug causing the plugin not working in reobfuscated environment

--- src/main/java/big_xplosion/mobeggs/MobEggs.java
+++ src/main/java/big_xplosion/mobeggs/MobEggs.java
@@ -3,40 +3,37 @@ package big_xplosion.mobeggs;
 import big_xplosion.mobeggs.config.Config;
 import big_xplosion.mobeggs.event.ListenerPlayerInteractEntity;
 import big_xplosion.mobeggs.lib.Constants;
-import big_xplosion.msu.core.helper.RegistryHelper;
 import com.google.inject.Inject;
 import org.spongepowered.api.Game;
 import org.spongepowered.api.config.ConfigDir;
 import org.spongepowered.api.event.Listener;
-import org.spongepowered.api.event.game.state.GameConstructionEvent;
+import org.spongepowered.api.event.game.state.GameInitializationEvent;
 import org.spongepowered.api.event.game.state.GamePreInitializationEvent;
 import org.spongepowered.api.plugin.Plugin;
 
 import java.nio.file.Path;
 
-@Plugin(id = Constants.PLUGIN_ID, name = Constants.PLUGIN_NAME, version = Constants.PLUGIN_VERSION, dependencies = "after:MSU|Core")
+@Plugin(id = Constants.PLUGIN_ID, name = Constants.PLUGIN_NAME, version = Constants.PLUGIN_VERSION, dependencies = "required-after:MSU|Core")
 public class MobEggs {
 
 	private static MobEggs plugin;
-	private Game game;
 
-	public RegistryHelper registry;
+	@Inject
+	private Game game;
 
 	@Inject
 	@ConfigDir(sharedRoot = false)
 	private Path configDir;
 
 	@Listener
-	public void onConstruction(GameConstructionEvent event) {
+	public void onPreInit(GamePreInitializationEvent event) {
 		plugin = this;
-		game = event.getGame();
-		registry = RegistryHelper.createRegistryHelper(plugin, game);
 		Config.init();
 	}
 
 	@Listener
-	public void onPrereInit(GamePreInitializationEvent event) {
-		registry.registerEventListener(new ListenerPlayerInteractEntity());
+	public void onInit(GameInitializationEvent event) {
+		getGame().getEventManager().registerListeners(plugin, new ListenerPlayerInteractEntity());
 	}
 
 	public static MobEggs getPlugin() {
